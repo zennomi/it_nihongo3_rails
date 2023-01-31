f4f3686bf590e342e315a50a7b8ea672c7d00884
zen-lenovo <zennomi.kun@gmail.com>
2023-01-31 18:46:37 +0700

    Add avatar on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_161441) do
+ActiveRecord::Schema.define(version: 2023_01_31_113723) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
